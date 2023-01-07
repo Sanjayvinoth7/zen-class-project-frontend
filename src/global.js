a624e7861e2fa04dea9b6c982fd5a2092eda7d26
Sanjayvinoth7 <110889214+Sanjayvinoth7@users.noreply.github.com>
2023-01-07 15:53:32 +0530

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,4 +1,4 @@
-// export const API = "http://localhost:4000";
+export const API = "http://localhost:4000";
 
 
-export const API = "https://zen-class-project-backend.onrender.com";
+//export const API = "https://zen-class-project-backend.onrender.com";
