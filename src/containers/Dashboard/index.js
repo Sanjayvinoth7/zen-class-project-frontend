c870d2b48761146b24e1023459a202dcdc37c654
Sanjayvinoth <110889214+Sanjayvinoth7@users.noreply.github.com>
2023-03-07 20:21:48 +0530

    Update index.js

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,7 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { API } from "../../global";
-import { CourseBarChart } from "./CourseBarChart";
 import { PieRechartComponent } from "./CoursePieChart";
 
 
